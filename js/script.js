b9ce21ea5827acbd78c38f606da44a5acadcb0eb
mmolodovskiy <mmolodovskiy@users.noreply.github.com>
2020-01-24 01:37:11 +0200

    smth

--- js/script.js
+++ js/script.js
@@ -1,22 +1,25 @@
-function shiftSlide() {
-    var leftArrow = document.getElementById("leftArrow");
-    var rightArrow = document.getElementById("rightArrow");
-    var sliderItems = document.getElementById("sliderItems");
-    var defaultPosition = 0;
-    var defaultShift = 20;
-    leftArrow.onclick = function() {
-        defaultPosition = parseInt(defaultPosition)-defaultShift+'%';
-        shiftPosition = sliderItems.style.left=defaultPosition;
-        return shiftPosition;
-    };
-    rightArrow.onclick = function() {
-        defaultPosition = parseInt(defaultPosition)+defaultShift+'%';
-        shiftPosition = sliderItems.style.left=defaultPosition;
-        return shiftPosition;
-    };
-}
+var leftArrow = document.getElementById("leftArrow");
+var rightArrow = document.getElementById("rightArrow");
+var sliderItems = document.getElementById("sliderItems");
+var defaultPosition = 0;
+var defaultShift = 20;
+leftArrow.onclick = function() {
+    defaultPosition = parseInt(defaultPosition)-defaultShift+'%';
+    shiftPosition = sliderItems.style.left=defaultPosition;
+    return shiftPosition;
+};
+rightArrow.onclick = function() {
+    defaultPosition = parseInt(defaultPosition)+defaultShift+'%';
+    shiftPosition = sliderItems.style.left=defaultPosition;
+    return shiftPosition;
+};
 
-shiftSlide();
+var rightSliderPos = sliderItems.getBoundingClientRect();
+//var some = sliderItems.lastChild.getBoundingClientRect();
+alert(rightSliderPos.right + "px");
+alert(rightSliderPos.left + pageXOffset + "px");
+
+
 
 
 //document.getElementById("sliderItems").children.length
